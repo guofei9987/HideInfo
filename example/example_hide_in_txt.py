92592d7b826e49231ebce8ef7e29e7c09bb93c91
guofei9987 <guofei9987@foxmail.com>
2023-09-13 21:32:24 +0800

    改名：text -> txt

--- example/example_hide_in_txt.py
+++ example/example_hide_in_txt.py
@@ -1,16 +1,16 @@
-from hide_info import hide_in_text
+from hide_info import hide_in_txt
 
 bytes_data = "待嵌入的句子".encode('utf-8')
 
 text = '一段文本，下面的代码中，这段文本会被嵌入隐藏信息。隐藏后的文本在MacBook等系统上是看不到差别的，但是 Windows 上能看到很多空格' * 50
-text_new = hide_in_text.encode(bytes_data, text)
-text_extract = hide_in_text.decode(text_new)
+text_new = hide_in_txt.encode(bytes_data, text)
+text_extract = hide_in_txt.decode(text_new)
 print(text_extract.decode('utf-8'))
 
 # %%把一个文件隐藏到文本中
-hide_in_text.file_encode(filename='要隐藏的文件2.zip', text_filename='一段文本.txt', text_filename_new='隐藏后的文本.txt')
+hide_in_txt.file_encode(filename='要隐藏的文件2.zip', text_filename='一段文本.txt', text_filename_new='隐藏后的文本.txt')
 
-hide_in_text.file_decode(filename='解出的文件.zip', text_filename='隐藏后的文本.txt')
+hide_in_txt.file_decode(filename='解出的文件.zip', text_filename='隐藏后的文本.txt')
 
 # 验证一致
 import hashlib
